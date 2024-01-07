9e10077937d490e11204e5a2f44d3fe938fd9d05
akebono03 <shin8gou@gmail.com>
2024-01-07 23:44:53 +0900

    template2

--- app/Main.hs
+++ app/Main.hs
@@ -2,10 +2,6 @@
 
 module Main (main) where
 
--- import Lib
--- main :: IO ()
--- main = someFunc
-
 import Web.Spock
 import Web.Spock.Config
 import Network.Wai.Middleware.Static (staticPolicy, addBase)
@@ -13,35 +9,28 @@ import qualified Data.Text.IO as TIO
 import Control.Monad.IO.Class (liftIO)
 import qualified Data.Text as T
 import Text.Read (readMaybe)
--- import Database.Persist.Sqlite
 import Database.SQLite.Simple (open, close, query_, field, FromRow(..), Connection)
 import Data.Aeson (ToJSON, toJSON, object, (.=))
+-- import Data.Maybe (fromMaybe)
 
--- data Person = Person
---   { personId :: Int
---   , personName :: String
---   , personAge :: Int
---   } deriving (Show)
 -- Sphere 型の定義
 data Sphere = Sphere
-  { sphereId :: Int
-  , sphereK :: Int
-  , sphereN :: Int
+  { sphereId :: Float
+  , sphereK :: Float
+  , sphereN :: Float
+  , orders :: Float
+  , generator :: String
+  , p :: Maybe String
+  , p_coe :: Maybe String
   -- その他のカラムに対応するフィールドを追加
   } deriving (Show)
 
 -- -- FromRow型クラスのインスタンスを作成
--- instance FromRow Person where
---   fromRow = Person <$> field <*> field <*> field
 instance FromRow Sphere where
-  fromRow = Sphere <$> field <*> field <*> field
+  fromRow = Sphere <$> field <*> field <*> field <*> field <*> field <*> field <*> field
   -- 必要に応じて、他のフィールドに対応する field を追加
 
 -- ToJSONインスタンスの定義
--- instance ToJSON Person where
---   -- toJSON (Person id name age) = object ["id" .= id, "name" .= name, "age" .= age]
---   -- toJSON (Person personId name age) = object ["id" .= personId, "name" .= name, "age" .= age]
---   toJSON p = object ["id" .= personId p, "name" .= personName p, "age" .= personAge p]
 instance ToJSON Sphere where
   toJSON s = object 
     [ "id" .= sphereId s
@@ -75,33 +64,34 @@ appWithConnection conn = do
 
   post "calculate" $ do
     formData <- params
+    spheres <- liftIO $ query_ conn "SELECT id, k, n, orders, generator, P, P_coe FROM sphere WHERE n = 13 and k = 0" :: ActionCtxT () (WebStateM () () ()) [Sphere]
     let n = maybe 0 (readInt . T.unpack) (lookup "n" formData)
         k = maybe 0 (readInt . T.unpack) (lookup "k" formData)
         result = n + k
-    html $ T.pack $ "n + k = " <> (show result)
+        htmlContent = generateHtmlForSphere spheres
+        htmlString = T.unpack htmlContent
+        -- ss = if htmlString == "" then "aaaa" else "bbbb"
+    -- html $ T.pack $ "n + k = " <> (show result) <> htmlString
+    -- 'template2.html' を読み込む
+    templateContent <- liftIO $ TIO.readFile "static/template2.html"
+    -- プレースホルダーを実際の値で置換
+    let finalHtml = T.replace "{result}" (T.pack $ show result) $
+                    T.replace "{sphereList}" (T.pack $ htmlString) templateContent
+    html finalHtml
     
-  -- -- データベースからクエリを実行して結果を取得
-  -- que <- liftIO $ query_ conn "SELECT * FROM sphere WHERE n = 3 and k = 8" :: ActionCtxT () (WebStateM () () ()) [Person]
-  -- ここでデータベースを利用するロジックを追加
-  get "people" $ do
-    spheres <- liftIO $ query_ conn "SELECT id, k, n FROM sphere WHERE n = 3 and k = 8" :: ActionCtxT () (WebStateM () () ()) [Sphere]
-    let htmlContent = generateHtmlForSphere spheres
-    html htmlContent
-    -- people <- liftIO $ query_ conn "SELECT * FROM people" :: ActionCtxT () (WebStateM () () ()) [Person]
-    -- people <- liftIO $ query_ conn "SELECT * FROM sphere WHERE n = 3 and k = 8" :: ActionCtxT () (WebStateM () () ()) [Person]
-    -- 取得したデータをレスポンスとして返す
-    -- json people
+--   -- ここでデータベースを利用するロジックを追加
+--   get "calculate" $ do
+--     spheres <- liftIO $ query_ conn "SELECT id, k, n, orders, generator FROM sphere WHERE n = 3 and k = 8" :: ActionCtxT () (WebStateM () () ()) [Sphere]
+--     let htmlContent = generateHtmlForSphere spheres
+--     html htmlContent
+--     -- 取得したデータをレスポンスとして返す
+--     -- json people
 
-    -- let htmlContent = generateHtml people
-    -- html htmlContent
 
 -- Sphere のリストを HTML に変換する関数
 generateHtmlForSphere :: [Sphere] -> T.Text
 generateHtmlForSphere spheres = T.concat
   -- HTMLを生成するコード
--- -- Personのリストを受け取り、HTMLを生成する関数
--- generateHtml :: [Person] -> T.Text
--- generateHtml people = T.concat
   [ "<html><head><title>Sphere</title></head><body>"
   , "<h1>Sphere List</h1>"
   , "<ul>"
@@ -110,50 +100,24 @@ generateHtmlForSphere spheres = T.concat
   , "</body></html>"
   ]
 
--- sphereToHtml :: Person -> T.Text
--- sphereToHtml (Person _ name age) =
---   T.concat ["<li>", T.pack name, " (", T.pack $ show age, " years old)</li>"]
 sphereToHtml :: Sphere -> T.Text
-sphereToHtml (Sphere ssphereId k n) =
-  T.concat ["<li>", "ID: ", T.pack $ show ssphereId, ", K: ", T.pack $ show k, ", N: ", T.pack $ show n, "</li>"]
+sphereToHtml (Sphere sId k n ord gene pp pp_coe) =
+  T.concat ["<li>", "ID: ", T.pack $ show sId, ", K: ", T.pack $ show k, ", N: ", T.pack $ show n, ", order: ", T.pack $ show ord, ", generator: ", T.pack $ show gene, ", P: ", T.pack $ stripQuotes (fmap show pp), ", P_coe: ", T.pack $ show pp_coe, "</li>"]
 
-  -- -- 取得した結果を表示
-  -- liftIO $ print que
+stripQuotes :: Maybe String -> String
+stripQuotes (Just str) = "\\(" ++ stripQuotesHelper str ++ "\\)"
+stripQuotes Nothing    = ""
 
--- -- メインのSpockアプリケーション
--- app :: SpockM () () () ()
--- -- app :: SpockM SqlBackend () ()
--- app = do
---   middleware (staticPolicy (addBase "static"))  -- 静的ファイルのミドルウェア
+stripQuotesHelper :: String -> String
+stripQuotesHelper str =
+  case str of
+    ('"':xs) -> case reverse xs of
+                  ('"':ys) -> reverse ys
+                  _        -> str
+    _        -> str
 
---   Web.Spock.get root $ do
---     -- テンプレートファイルを読み込み、レスポンスに埋め込む
---     templateContent <- liftIO $ TIO.readFile "static/template.html"
---     html templateContent
---   post "calculate" $ do
---     formData <- params
---     let n = maybe 0 (readInt . T.unpack) (lookup "n" formData)
---         k = maybe 0 (readInt . T.unpack) (lookup "k" formData)
---         result = n + k
---     html $ T.pack $ "n + k = " <> (show result)      
 
--- -- Spockアプリケーションの設定
--- spockCfg :: SpockCfg () () ()
--- spockCfg = defaultSpockCfg () PCNoDatabase ()
 
--- -- メインのSpockアプリケーション
--- app :: SpockM () () () ()
--- app = spock spockCfg $ do
---   middleware (staticPolicy (addBase "static"))  -- 静的ファイルのミドルウェア
 
---   get root $ do
---     -- テンプレートファイルを読み込み、レスポンスに埋め込む
---     -- templateContent <- liftIO $ TIO.readFile "template.html"
---     templateContent <- liftIO $ TIO.readFile "static/template.html"
 
---     html templateContent
-
--- main :: IO ()
--- main = do
---   runSpock 8080 app
 
